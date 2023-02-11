0c71e026b9e8f2d0b0926bba02480c2c24889627
Felipe Manchester <f.p2809@gmail.com>
2023-02-11 11:59:35 -0300

    feat: pause storie

--- src/Slide.ts
+++ src/Slide.ts
@@ -1,17 +1,117 @@
+import Timeout from './Timeout.js';
+
 export default class Slide {
   container;
-  elements;
+  slides;
   controls;
   time;
+  index;
+  slide;
+  timeout: Timeout | null;
+  pausedTimeout: Timeout | null;
+  paused: boolean;
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
     time: number = 5000,
   ) {
     this.container = container;
-    this.elements = elements;
+    this.slides = slides;
     this.controls = controls;
     this.time = time;
+    this.index = localStorage.getItem('activeSlide')
+      ? Number(localStorage.getItem('activeSlide'))
+      : 0;
+    this.slide = this.slides[this.index];
+    this.timeout = null;
+    this.paused = false;
+    this.pausedTimeout = null;
+    this.init();
+  }
+
+  pause() {
+    this.pausedTimeout = new Timeout(() => {
+      this.timeout?.pause();
+      this.paused = true;
+      if (this.slide instanceof HTMLVideoElement) this.slide.pause();
+    }, 300);
+  }
+
+  continue() {
+    this.pausedTimeout?.clear();
+    if (this.paused) {
+      this.paused = false;
+      this.timeout?.continue();
+      if (this.slide instanceof HTMLVideoElement) this.slide.play();
+    }
+  }
+
+  hide(element: Element) {
+    element.classList.remove('active');
+    if (element instanceof HTMLVideoElement) {
+      element.currentTime = 0;
+      element.pause();
+    }
+  }
+  show(index: number) {
+    this.index = index;
+    this.slide = this.slides[this.index];
+    localStorage.setItem('activeSlide', String(this.index));
+
+    this.slides.forEach((element) => this.hide(element));
+    this.slide.classList.add('active');
+
+    if (this.slide instanceof HTMLVideoElement) {
+      this.autoVideo(this.slide);
+    } else {
+      this.auto(this.time);
+    }
+  }
+  autoVideo(video: HTMLVideoElement) {
+    video.muted = true;
+    video.play();
+    let firstPlay = true;
+    video.addEventListener('playing', () => {
+      if (firstPlay) this.auto(video.duration * 1000);
+      firstPlay = false;
+    });
+    // button onclick video muted = false
+  }
+  auto(time: number) {
+    this.timeout?.clear();
+    this.timeout = new Timeout(() => this.next(), time);
+  }
+  prev() {
+    if (this.paused) return;
+    const prev = this.index > 0 ? this.index - 1 : this.slides.length - 1;
+    this.show(prev);
+  }
+
+  next() {
+    if (this.paused) return;
+    const next = this.index + 1 < this.slides.length ? this.index + 1 : 0;
+    this.show(next);
+  }
+
+  private addControls() {
+    const prevButton = document.createElement('button');
+    const nextButton = document.createElement('button');
+    prevButton.innerText = 'Anterior';
+    nextButton.innerText = 'Próximo';
+
+    this.controls.appendChild(prevButton);
+    this.controls.appendChild(nextButton);
+
+    this.controls.addEventListener('pointerdown', () => this.pause());
+    this.controls.addEventListener('pointerup', () => this.continue());
+
+    prevButton.addEventListener('pointerup', () => this.prev());
+    nextButton.addEventListener('pointerup', () => this.next());
+  }
+
+  private init() {
+    this.addControls();
+    this.show(this.index);
   }
 }
